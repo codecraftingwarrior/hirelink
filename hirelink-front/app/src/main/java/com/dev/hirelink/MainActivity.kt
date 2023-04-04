6280a553f81307255f3f9f30360ada0a02f8393a
El Hadji Ahmadou GUEYE <128684816+codecraftingwarrior@users.noreply.github.com>
2023-04-04 16:15:50 +0200

    implemented first screen & installer dependencies

---
+++ hirelink-front/app/src/main/java/com/dev/hirelink/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.dev.hirelink
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+    }
+}
