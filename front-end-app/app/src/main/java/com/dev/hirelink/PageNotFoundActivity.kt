8b3d826390fc0a365f59322191552697bee1694a
braham01 <braham.hmed.1998@gmail.com>
2023-04-06 16:37:05 +0200

    added page_not_found_activity + employer_register_fragments(step1 & step2)

---
+++ front-end-app/app/src/main/java/com/dev/hirelink/PageNotFoundActivity.kt
@@ -0,0 +1,11 @@
+package com.dev.hirelink
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PageNotFoundActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_page_not_found)
+    }
+}
