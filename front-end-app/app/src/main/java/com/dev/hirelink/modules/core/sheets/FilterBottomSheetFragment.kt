e37cdaac9c47d62b07bf9a1816da75730685405a
El Hadji Ahmadou GUEYE <128684816+codecraftingwarrior@users.noreply.github.com>
2023-04-11 19:09:31 +0200

    added ripple effect to bottom sheet & centering step fragments

--- front-end-app/app/src/main/java/com/dev/hirelink/modules/core/sheets/FilterBottomSheetFragment.kt
+++ front-end-app/app/src/main/java/com/dev/hirelink/modules/core/sheets/FilterBottomSheetFragment.kt
@@ -1,26 +1,32 @@
 package com.dev.hirelink.modules.core.sheets
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.dev.hirelink.R
+import com.dev.hirelink.databinding.FragmentBottomSheetFilterBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 
 class FilterBottomSheetFragment : BottomSheetDialogFragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var binding: FragmentBottomSheetFilterBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bottom_sheet_filter, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_bottom_sheet_filter, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.download.setOnClickListener { Log.d(TAG, "item download clicked") }
     }
 
     companion object {
