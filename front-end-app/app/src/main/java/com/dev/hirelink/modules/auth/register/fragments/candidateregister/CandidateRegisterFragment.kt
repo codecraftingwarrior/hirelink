d95f13c0c1bf5e7a45a6f86f01557eec2c65c3c3
El Hadji Ahmadou GUEYE <128684816+codecraftingwarrior@users.noreply.github.com>
2023-04-20 17:13:57 +0200

    added validation to applicant registration fragment

--- front-end-app/app/src/main/java/com/dev/hirelink/modules/auth/register/fragments/candidateregister/CandidateRegisterFragment.kt
+++ front-end-app/app/src/main/java/com/dev/hirelink/modules/auth/register/fragments/candidateregister/CandidateRegisterFragment.kt
@@ -2,6 +2,8 @@ package com.dev.hirelink.modules.auth.register.fragments.candidateregister
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -35,7 +37,9 @@ class CandidateRegisterFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.buttonRegister.isEnabled = false
         binding.buttonRegister.setOnClickListener { listener.onRegistrationTerminated() }
+        attachTextWatchers()
     }
 
     override fun onAttach(context: Context) {
@@ -44,6 +48,34 @@ class CandidateRegisterFragment : Fragment() {
             listener = context as RegistrationTerminationListener
         } catch (e: java.lang.Exception) {
             throw Exception("$context must implement ApplicantRegistrationTerminationListener")
+        }
+    }
+
+    private fun attachTextWatchers() {
+        with(binding) {
+
+            val watcher = object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+                override fun afterTextChanged(p0: Editable?) {}
+
+                override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    buttonRegister.isEnabled =
+                        editTextFirstName.text!!.isNotEmpty()
+                                && editTextLastName.text!!.isNotEmpty()
+                                && editTextPhoneNumber.text!!.isNotEmpty()
+                                && editTextEmail.text!!.isNotEmpty()
+                                && editTextPassword.text!!.isNotEmpty()
+                                && editTextPasswordConfirm.text!!.isNotEmpty()
+                }
+            }
+
+            editTextFirstName.addTextChangedListener(watcher)
+            editTextLastName.addTextChangedListener(watcher)
+            editTextPhoneNumber.addTextChangedListener(watcher)
+            editTextEmail.addTextChangedListener(watcher)
+            editTextPassword.addTextChangedListener(watcher)
+            editTextPasswordConfirm.addTextChangedListener(watcher)
         }
     }
 
