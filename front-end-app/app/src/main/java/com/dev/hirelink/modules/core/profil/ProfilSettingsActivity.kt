ea5d89d958852de4c2af76b4908905612121496c
Siooxx <120092224+Siooxx@users.noreply.github.com>
2023-04-20 18:16:21 +0200

    addind activity_profil_setting

---
+++ front-end-app/app/src/main/java/com/dev/hirelink/modules/core/profil/ProfilSettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.dev.hirelink.modules.core.profil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dev.hirelink.R
+
+class ProfilSettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil_settings)
+    }
+}
