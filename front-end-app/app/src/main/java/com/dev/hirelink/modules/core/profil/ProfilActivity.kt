1044e5d3bee5530e8905b627f7d9d56bee7d0639
Siooxx <120092224+Siooxx@users.noreply.github.com>
2023-04-20 17:30:34 +0200

    Ajusting activity_profil

--- front-end-app/app/src/main/java/com/dev/hirelink/modules/core/profil/ProfilActivity.kt
+++ front-end-app/app/src/main/java/com/dev/hirelink/modules/core/profil/ProfilActivity.kt
@@ -2,11 +2,11 @@ package com.dev.hirelink.modules.core.profil
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.dev.hirelink.R
 import com.dev.hirelink.databinding.ActivityProfilBinding
 
 class ProfilActivity : AppCompatActivity() {
     private lateinit var binding: ActivityProfilBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProfilBinding.inflate(layoutInflater)
