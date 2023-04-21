2a5cd599fb6abfa672eaf99fca91ae2ec434287b
El Hadji Ahmadou GUEYE <128684816+codecraftingwarrior@users.noreply.github.com>
2023-04-21 13:44:23 +0200

    implemented job application list fragment

--- front-end-app/app/src/main/java/com/dev/hirelink/models/JobApplication.kt
+++ front-end-app/app/src/main/java/com/dev/hirelink/models/JobApplication.kt
@@ -1,6 +1,8 @@
 package com.dev.hirelink.models
 
 data class JobApplication(
-    val id: Int
-) {
-}
+    val id: Int? = null,
+    val state: String? = null,
+    val jobOffer: JobOffer? = null,
+    val applicant: ApplicationUser? = null
+)
