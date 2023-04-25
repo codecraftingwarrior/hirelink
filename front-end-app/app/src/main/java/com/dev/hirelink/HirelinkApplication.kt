685b1842587f3f06e6d6b871f2810a0601c6018e
Siooxx <120092224+Siooxx@users.noreply.github.com>
2023-04-25 17:43:38 +0200

    Ajout de commentaires dans le code

--- front-end-app/app/src/main/java/com/dev/hirelink/HirelinkApplication.kt
+++ front-end-app/app/src/main/java/com/dev/hirelink/HirelinkApplication.kt
@@ -3,6 +3,10 @@ package com.dev.hirelink
 import android.app.Application
 import com.dev.hirelink.network.auth.AuthRepository
 
+// Classe HirelinkApplication héritant de la classe Application
 class HirelinkApplication : Application() {
+
+    // Propriété authRepository de type AuthRepository, initialisée avec un objet de type AuthRepository
+    // Utilise le mécanisme 'lazy' pour n'instancier l'objet qu'au moment de la première utilisation
     val authRepository: AuthRepository by lazy { AuthRepository(applicationContext) }
 }
--- front-end-app/app/src/main/java/com/dev/hirelink/MainActivity.kt
+++ front-end-app/app/src/main/java/com/dev/hirelink/MainActivity.kt
@@ -9,19 +9,29 @@ import com.dev.hirelink.modules.auth.login.LoginActivity
 import com.dev.hirelink.modules.auth.register.RegisterActivity
 import com.dev.hirelink.modules.core.BaseActivity
 
+// Classe MainActivity héritant de AppCompatActivity
 class MainActivity : AppCompatActivity() {
+
+    // Méthode onCreate appelée lors de la création de l'activité
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Définition du layout de l'activité
         setContentView(R.layout.activity_main)
 
+        // Cache la barre d'action
         supportActionBar?.hide()
 
+        // Appelle de la méthode bindListeners pour associer les écouteurs d'événements
         bindListeners();
     }
 
+    // Méthode pour associer les écouteurs d'événements aux boutons
     private fun bindListeners() {
+        // Bouton d'inscription
         val registerButton = findViewById<AppCompatButton>(R.id.button_register)
         registerButton.setOnClickListener {
+            // Naviguer vers l'activité RegisterActivity
             startActivity(
                 Intent(
                     applicationContext,
@@ -30,8 +40,10 @@ class MainActivity : AppCompatActivity() {
             )
         }
 
+        // Bouton de connexion
         val loginButton = findViewById<AppCompatButton>(R.id.button_login)
         loginButton.setOnClickListener {
+            // Naviguer vers l'activité LoginActivity
             startActivity(
                 Intent(
                     applicationContext,
@@ -40,8 +52,10 @@ class MainActivity : AppCompatActivity() {
             )
         }
 
+        // Bouton pour afficher les offres
         val viewOffersButton = findViewById<Button>(R.id.button_view_offers)
         viewOffersButton.setOnClickListener {
+            // Naviguer vers l'activité BaseActivity
             startActivity(
                 Intent(
                     this,
