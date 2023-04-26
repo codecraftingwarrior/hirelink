b820706b2420f38d3e3128b4b89bde2d5bb69642
El Hadji Ahmadou GUEYE <128684816+codecraftingwarrior@users.noreply.github.com>
2023-04-26 18:42:46 +0200

    added mapbox api and address autofill feature

--- front-end-app/app/src/main/java/com/dev/hirelink/modules/auth/register/fragments/employerregister/EmployerRegisterStep1Fragment.kt
+++ front-end-app/app/src/main/java/com/dev/hirelink/modules/auth/register/fragments/employerregister/EmployerRegisterStep1Fragment.kt
@@ -1,23 +1,34 @@
 package com.dev.hirelink.modules.auth.register.fragments.employerregister
 
-import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.dev.hirelink.R
 import com.dev.hirelink.databinding.FragmentEmployerRegisterStep1Binding
 import com.dev.hirelink.enums.RegistrationStep
 import com.dev.hirelink.modules.auth.register.fragments.StepFragment
+import com.mapbox.search.autofill.AddressAutofill
+import com.mapbox.search.autofill.AddressAutofillSuggestion
+import com.mapbox.search.autofill.Query
+import com.mapbox.search.ui.adapter.autofill.AddressAutofillUiAdapter
+import com.mapbox.search.ui.view.CommonSearchViewConfiguration
+import com.mapbox.search.ui.view.DistanceUnitType
+import com.mapbox.search.ui.view.SearchResultsView
 
 
 class EmployerRegisterStep1Fragment : StepFragment() {
     private lateinit var binding: FragmentEmployerRegisterStep1Binding
     private val currentStep = RegistrationStep.STEP_1
+    private lateinit var addressAutofill: AddressAutofill
+    private lateinit var searchResultsView: SearchResultsView
+    private lateinit var addressAutofillUiAdapter: AddressAutofillUiAdapter
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,10 +46,61 @@ class EmployerRegisterStep1Fragment : StepFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.buttonNext.setOnClickListener { createAccount() }
         attachTextWatchers()
+        setupAddressAutofillField()
     }
 
     private fun createAccount() {
         listener.onNextButtonTouched(currentStep)
+    }
+
+    private fun setupAddressAutofillField() {
+        addressAutofill = AddressAutofill.create(getString(R.string.mapbox_access_token))
+
+        searchResultsView = binding.searchResultsView
+
+        searchResultsView.initialize(
+            SearchResultsView.Configuration(
+                commonConfiguration = CommonSearchViewConfiguration(DistanceUnitType.IMPERIAL)
+            )
+        )
+
+        addressAutofillUiAdapter = AddressAutofillUiAdapter(
+            view = searchResultsView,
+            addressAutofill = addressAutofill
+        )
+
+        addressAutofillUiAdapter.addSearchListener(object : AddressAutofillUiAdapter.SearchListener {
+
+            override fun onSuggestionSelected(suggestion: AddressAutofillSuggestion) {
+                showAddressAutofillSuggestion(
+                    suggestion,
+                    fromReverseGeocoding = false,
+                )
+            }
+
+            override fun onSuggestionsShown(suggestions: List<AddressAutofillSuggestion>) {}
+
+            override fun onError(e: Exception) {}
+        })
+
+        binding.editTextPostalAddress.addTextChangedListener(object : TextWatcher {
+
+            override fun onTextChanged(text: CharSequence, start: Int, before: Int, count: Int) {
+
+                val query = Query.create(text.toString())
+                if (query != null) {
+                    lifecycleScope.launchWhenStarted {
+                        addressAutofillUiAdapter.search(query)
+                    }
+                }
+                searchResultsView.isVisible = query != null
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) { }
+
+            override fun afterTextChanged(s: Editable) { }
+        })
+
     }
 
     private fun attachTextWatchers() {
@@ -65,6 +127,21 @@ class EmployerRegisterStep1Fragment : StepFragment() {
             editTextCity.addTextChangedListener(watcher)
             editTextCountry.addTextChangedListener(watcher)
         }
+    }
+
+    private fun showAddressAutofillSuggestion(suggestion: AddressAutofillSuggestion, fromReverseGeocoding: Boolean) {
+        val address = suggestion.result().address
+        binding.editTextCity.setText(address.place)
+        binding.editTextCountry.setText(address.country)
+        binding.editTextPostalAddress.setText(
+            listOfNotNull(
+                address.houseNumber,
+                address.street
+            ).joinToString()
+        )
+        binding.editTextPostalAddress.clearFocus()
+
+        searchResultsView.isVisible = false
     }
 
 }
